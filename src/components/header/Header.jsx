0aac53e5099733741d308def626eb055b4f645ed
tejasbaid <tejasbaid3@gmail.com>
2020-08-13 12:40:24 +0530

    Initial Commit

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Header.scss'
+
+export const Header = () => {
+    return (
+        <div className="header">
+            <div className="logo">
+                <h1 className='logo-text'>Github <span>Jobs</span> </h1>
+            </div>
+        </div>
+    )
+}
---
+++ src/components/searchbar/Searchbar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Searchbar.scss'
+
+export const Searchbar = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
